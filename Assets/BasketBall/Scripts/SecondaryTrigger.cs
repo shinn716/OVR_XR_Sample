8d882415b5882c4c637e7caed8d404120e994c06
shinn716 <shinn716@livemail.tw>
2024-04-25 22:29:37 +0800

    add basketball assets.

---
+++ Assets/BasketBall/Scripts/SecondaryTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecondaryTrigger : MonoBehaviour {
+
+    Collider expectedCollider;
+
+	public void ExpectCollider(Collider collider)
+    {
+        expectedCollider = collider;
+    }
+
+    void OnTriggerEnter(Collider otherCollider)
+    {
+        //print("1");
+        if (otherCollider == expectedCollider)
+        {
+            //print("Trigger Entered");
+            ScoreKeeper scoreKeeper = FindObjectOfType<ScoreKeeper>();
+            scoreKeeper.IncrementScore(1);
+        }
+    }
+}
