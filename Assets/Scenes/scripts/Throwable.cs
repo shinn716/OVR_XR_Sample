3b48535a753fb3ee8756bb4b6d530d662a7b9208
DELTA\JOHN.TC.TSAI <john.tc.tsai@deltaww.com>
2024-05-27 09:06:07 +0800

    feat: Modify throwable.

--- Assets/Scenes/scripts/Throwable.cs
+++ Assets/Scenes/scripts/Throwable.cs
@@ -6,54 +6,89 @@ using Oculus.Interaction;
 using Oculus.Interaction.HandGrab;
 using UnityEngine;
 
-[RequireComponent(typeof(Grabbable))]
-[RequireComponent(typeof(HandGrabInteractable))]
 public class Throwable : MonoBehaviour
 {
-    [SerializeField] float velocity = 1000f;
+    // [SerializeField] float velocity = 1000f;
 
     bool pickUp = false;
     List<Vector3> trackingPos = new List<Vector3>();
     Rigidbody rb;
-    Grabbable grabbable;
-    HandGrabInteractable handGrab;
+    [SerializeField] Grabbable grabbable;
+    [SerializeField] HandGrabInteractable handGrab;
 
     public event Action eventPickup;
     public event Action eventThrow;
+
+    private Vector3 previousPosition;
+    private Vector3 velocity;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        handGrab = GetComponent<HandGrabInteractable>();
-        grabbable = GetComponent<Grabbable>();
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (pickUp && handGrab.State == InteractableState.Select)
-        {
-            if (trackingPos.Count > 5)
-                trackingPos.RemoveAt(0);
-            trackingPos.Add(transform.position);
-        }
+
+
+        // print(pickUp + " " + grabbable.Transform.name + " " + name);
+        // if ((OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger) || OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) ||
+        //    OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger) || OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger))
+        //     && handGrab.State == InteractableState.Select && grabbable.Transform.name == name
+        // )
+        // {
+        //     print("=====pickup=====");
+        //     pickUp = true;
+        //     previousPosition = transform.position;
+        // }
+
+        // if (pickUp && handGrab.State == InteractableState.Normal)
+        // {
+        //     pickUp = false;
+        // }
+
+        // print(handGrab.State);
+
+
+        // if (pickUp && handGrab.State == InteractableState.Select)
+        // {
+        //     // pickUp = true;
+        //     // if (trackingPos.Count > 5)
+        //     //     trackingPos.RemoveAt(0);
+        //     // trackingPos.Add(transform.position);
+        // }
         if (pickUp && handGrab.State == InteractableState.Normal)
         {
             print("throw");
-            eventThrow?.Invoke();
-            Vector3 direction = trackingPos[trackingPos.Count - 1] - trackingPos[0];
-            rb.AddForce(velocity * direction);
+            pickUp = false;
+            rb.velocity = velocity * 3; // Apply the calculated velocity
+            // eventThrow?.Invoke();
+            // Vector3 direction = trackingPos[trackingPos.Count - 1] - trackingPos[0];
+            // rb.AddForce(velocity * direction);
         }
     }
 
+    private void FixedUpdate()
+    {
+        if (pickUp)
+        {
+            // 根据位置变化计算速度
+            velocity = (transform.position - previousPosition) / Time.fixedDeltaTime;
+            previousPosition = transform.position;
+        }
+    }
+
+
     void OnTriggerStay(Collider other)
     {
-        if (!pickUp && grabbable.Transform.name == name && handGrab.State == InteractableState.Select)
+        // print(handGrab.State);
+        if (!pickUp && handGrab.State == InteractableState.Select)
         {
             print("pick");
-            eventPickup?.Invoke();
+            // eventPickup?.Invoke();
             pickUp = true;
         }
     }
